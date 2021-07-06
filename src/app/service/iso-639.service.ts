d4b496b6054ed2ad9e2c70cedc516fa3731518ba
Theo Rozier <contact@theorozier.fr>
2021-07-06 17:53:35 +0200

    Triage des langages par leur nom visuel.

--- src/app/service/iso-639.service.ts
+++ src/app/service/iso-639.service.ts
@@ -41,6 +41,9 @@ export class Iso639Service {
 						langages.push(lang);
 						langagesAssociated[lang.code] = lang;
 					}
+					langages.sort((a, b) => {
+						return a.printName.localeCompare(b.printName);
+					});
 					resolve({
 						list: langages,
 						associated: langagesAssociated
